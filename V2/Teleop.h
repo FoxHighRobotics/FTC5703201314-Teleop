0ad4e94a9cd3740be00fab930de652cbc2257fe4
tylerfilla <tylerfilla@gmail.com>
2014-07-22 16:06:21 -0500

    Added Teleop V2 (as of 3:22 PM CST 12/17/2013)

---
+++ V2/Teleop.h
@@ -0,0 +1,22 @@
+
+// The Doppler Effect 2013
+// Teleop 2 - Header
+
+// Motor constants
+#define MOTOR_DRIVE_HIGH_SPEED 100
+#define MOTOR_DRIVE_LOW_SPEED 50
+
+// Servo constants
+#define SERVO_GRIPPER_OPEN_POS 90
+#define SERVO_GRIPPER_CLOSED_POS 0
+#define SERVO_ARM_SEGMENT_SPEED 0.5
+#define SERVO_ROTATE_SPEED 75
+
+// Button constants
+#define BUTTON_R1 6
+#define BUTTON_A 2
+#define BUTTON_B 3
+
+// Task prototypes
+task controller();
+task screen();
